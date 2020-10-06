6187b5cef376b61e68c9ec195c7c0b44854b3a0f
pan_fera <pan_fera@mail.ru>
2020-10-06 14:09:03 +0300

    v4.3

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(unit_tests "/home/hacker/Study/Programming_3sem/LR1/lab01-parser/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/hacker/Study/Programming_3sem/LR1/lab01-parser/CMakeLists.txt;51;add_test;/home/hacker/Study/Programming_3sem/LR1/lab01-parser/CMakeLists.txt;0;")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/hacker/Study/Programming_3sem/LR1/lab01-parser/CMakeLists.txt;54;add_test;/home/hacker/Study/Programming_3sem/LR1/lab01-parser/CMakeLists.txt;0;")
